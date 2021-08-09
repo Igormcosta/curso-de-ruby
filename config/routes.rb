f9324c8961fbe21851324d01ce17122b93fed170
Igor_Medeiros <igormedeiros1999@gmail.com>
2021-08-09 10:35:39 -0300

    new "help" page with a "help" button image

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root "pages#home"
+  root "pages#help"
   get "/about", to: "pages#about"
+  get "/help", to: "pages#help"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
